11ddb0b2a0e5a2e869b705454dfdd20370bae1c6
Roberto-Parra <w.parrav@uniandes.edu.co>
2022-05-22 14:28:48 -0400

    Test login

--- pool-dinamico/cypress/integration/Login.spec.js
+++ pool-dinamico/cypress/integration/Login.spec.js
@@ -1,13 +1,47 @@
 const baseUrl = Cypress.config("baseUrl")
 let username = Cypress.config('user')
 let password = Cypress.config('pwd')
+import dataset from '../fixtures/apriori/userlogin.json'
+
 beforeEach(() => {
     cy.visit('/#/signin')
 })
 describe('Testing Ghost Login', () =>{
-    it('Logs in with provided credentials', () => {
+    it('SC01 - Usuario y Password Correctos', () => {
         cy.get('.email').clear().type(username);
         cy.get('.password').clear().type(password);
         cy.get('.login').click()
-      });
+    });
+    it('SC02 - Usuario random y Password vacio', () => {
+        let dataRow;
+        let index = Math.floor(Math.random() * dataset.length  )
+        dataRow = dataset[index]
+        cy.get('.email').clear().type(dataRow.username);
+        //cy.get('.password').clear().type(dataRow.password);
+        cy.get('.login').click()
+    });
+    it('SC03 - Usuario vacio y Password random', () => {
+        let dataRow;
+        let index = Math.floor(Math.random() * dataset.length  )
+        dataRow = dataset[index]
+        //cy.get('.email').clear().type(dataRow.username);
+        cy.get('.password').clear().type(dataRow.password);
+        cy.get('.login').click()
+    });
+    it('SC04 - Usuario y Password random', () => {
+        let dataRow;
+        let index = Math.floor(Math.random() * dataset.length  )
+        dataRow = dataset[index]
+        cy.get('.email').clear().type(dataRow.username);
+        cy.get('.password').clear().type(dataRow.password);
+        cy.get('.login').click()
+    });
+    it('SC05 - Usuario y Password vacios', () => {
+        let dataRow;
+        let index = Math.floor(Math.random() * dataset.length  )
+        dataRow = dataset[index]
+        //cy.get('.email').clear().type(dataRow.username);
+        //cy.get('.password').clear().type(dataRow.password);
+        cy.get('.login').click()
+    });
 })
---
+++ pool-dinamico/cypress/integration/Profile.spec.js
@@ -0,0 +1,22 @@
+const baseUrl = Cypress.config("baseUrl")
+let username = Cypress.config('user')
+let password = Cypress.config('pwd')
+import dataset from '../fixtures/apriori/userlogin.json'
+
+beforeEach(() => {
+    cy.visit('/#/signin')
+})
+
+
+
+describe('Testing Ghost Profile', () =>{
+    it('SC01 - Dejar el nombre en blanco', () => {
+        cy.get('.email').clear().type(username);
+        cy.get('.password').clear().type(password);
+        cy.get('.login').click();
+        //cy.wait(200);
+        cy.visit('/#/settings/staff/tester/');
+        cy.get('.user-name ember-text-field gh-input ember-view').clear().type('hola');
+    });
+
+})
