d900c0de1d919696bebfa4600e2fa8b217e380a4
Roberto-Parra <w.parrav@uniandes.edu.co>
2022-05-22 17:11:37 -0400

    name in profile

--- pool-dinamico/cypress/integration/Profile.spec.js
+++ pool-dinamico/cypress/integration/Profile.spec.js
@@ -8,20 +8,71 @@ beforeEach(() => {
 })
 
 
-
 describe('Testing Ghost Profile', () =>{
-    it('SC01 - Dejar el nombre en blanco', () => {
+    it('SC01 - Colocar Nombre Aleatoriamente', () => {
         cy.get('.email').clear().type(username);
         cy.get('.password').clear().type(password);
         cy.get('.login').click();
         cy.wait(1000);
-        cy.get('a[href="#/settings/"]').click();
-        cy.wait(200);
-        cy.get('a[href="#/settings/staff/"]').click();
-        cy.contains("a","#/settings/staff/").click();
-        //cy.get('a[href="#/settings/staff/*."]').click();
-        //cy.visit('/#/settings/staff/tester/');
-        //cy.get('.user-name ember-text-field gh-input ember-view').clear().type('hola');
+        cy.get('div.gh-nav-bottom div[role="button"]').click();
+        cy.wait(1000);
+        cy.contains('Your profile').click();
+        cy.wait(1000);
+        cy.get('input#user-name').clear().type('hola', {force: true});
+        cy.get('#ember67 > span').click();
+        cy.get('.gh-canvas-title').should('contain', 'hola')
+    });
+    it('SC02 - Dejar el Nombre en Blanco', () => {
+        cy.get('.email').clear().type(username);
+        cy.get('.password').clear().type(password);
+        cy.get('.login').click();
+        cy.wait(1000);
+        cy.get('div.gh-nav-bottom div[role="button"]').click();
+        cy.wait(1000);
+        cy.contains('Your profile').click();
+        cy.wait(1000);
+        cy.get('input#user-name').clear();
+        cy.get('#ember67 > span').click();
+        cy.get('#ember71 > .response').contains('Please enter a name.')
+    });
+    it('SC03 - Dejar el Nombre solo números', () => {
+            cy.get('.email').clear().type(username);
+            cy.get('.password').clear().type(password);
+            cy.get('.login').click();
+            cy.wait(1000);
+            cy.get('div.gh-nav-bottom div[role="button"]').click();
+            cy.wait(1000);
+            cy.contains('Your profile').click();
+            cy.wait(1000);
+            cy.get('input#user-name').clear().type(123, {force: true});
+            cy.get('#ember67 > span').click();
+            cy.get('.gh-canvas-title').should('contain', 123)
+        });
+    it('SC04 - Dejar el Nombre largo', () => {
+        cy.get('.email').clear().type(username);
+        cy.get('.password').clear().type(password);
+        cy.get('.login').click();
+        cy.wait(1000);
+        cy.get('div.gh-nav-bottom div[role="button"]').click();
+        cy.wait(1000);
+        cy.contains('Your profile').click();
+        cy.wait(1000);
+        cy.get('input#user-name').clear().type('MDCCYUQLCWBBMVHSVPHLTDQCLHYCQBJMNEFDDDHURPYEFTRTJOMWLNVPIYAGKFIOWJEGPZTLPKCIDIZGEGBKHZRQICNWQGDUODRXIIZWZZZFFUSNDBORAHETOILYYPUTDTBOFSXSKBRDRRGPAWMHGAJZSUZEWBTPYTKDOHPAIYLZYTOHZUYDGYFCSPLULQELAIOVAHTIYNSHVNZHYMSJYWDEUZHGRDDBUNKMWYAEJBJVXFSFBBWHZOUWDZCEWSPXXDSQIMIWPEKKFCRSIZAJIXNCFAYXDGMZACFUYOLIVTJT6055R', {force: true});
+        cy.get('#ember67 > span').click();
+        cy.get('.gh-canvas-title').should('contain', 'MDCCYUQLCWBBMVHSVPHLTDQCLHYCQBJMNEFDDDHURPYEFTRTJOMWLNVPIYAGKFIOWJEGPZTLPKCIDIZGEGBKHZRQICNWQGDUODRXIIZWZZZFFUSNDBORAHETOILYYPUTDTBOFSXSKBRDRRGPAWMHGAJZSUZEWBTPYTKDOHPAIYLZYTOHZUYDGYFCSPLULQELAIOVAHTIYNSHVNZHYMSJYWDEUZHGRDDBUNKMWYAEJBJVXFSFBBWHZOUWDZCEWSPXXDSQIMIWPEKKFCRSIZAJIXNCFAYXDGMZACFUYOLIVTJT6055R')
+    });
+    it('SC05 - Dejar el Nombre corto', () => {
+        cy.get('.email').clear().type(username);
+        cy.get('.password').clear().type(password);
+        cy.get('.login').click();
+        cy.wait(1000);
+        cy.get('div.gh-nav-bottom div[role="button"]').click();
+        cy.wait(1000);
+        cy.contains('Your profile').click();
+        cy.wait(1000);
+        cy.get('input#user-name').clear().type('h', {force: true});
+        cy.get('#ember67 > span').click();
+        cy.get('.gh-canvas-title').should('contain', 'h')
     });
 
 })
